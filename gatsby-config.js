f3ed7ca02832b2845d1ad9991635e001f002c88b
RRoggia <renan.roggia@taglivros.com.br>
2021-03-28 00:43:19 -0300

    add configuration necessary

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,30 @@ module.exports = {
         'trackingId': 'UA-150419482-1',
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto`,
+        ],
+        display: 'swap'
+      }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: '/images/'
+        }
+      }
+    }
   ],
 }
